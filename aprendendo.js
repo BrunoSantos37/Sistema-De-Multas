a595722ac041b150b03925c97708860dc072126d
Bruno Candido <bruno.candido.santos@escola.pr.gov.br>
2023-02-07 15:25:28 -0300

    first commit

---
+++ aprendendo.js
@@ -0,0 +1,14 @@
+var inputNumber = document.querySelector('input#inputNumber');
+var inputButton = document.querySelector('input#inputButton');
+var resultado = document.querySelector('div.result')
+
+function calcular(){
+    var vel = Number(inputNumber.value);
+    if(vel > 60){
+        resultado.innerHTML = `Sua velocidade foi de ${vel}Km/h! Você passou da velocidade mínima exigida! (60km/h). Foram adicionados 5 pontos na sua carteira! `;
+        // alert('ai nao em')
+    }else{
+        resultado.innerHTML = `Use sempre os cintos de segurança! Se beber não dirija! Boa viagem!`
+        // alert('parabens')
+    }
+}
